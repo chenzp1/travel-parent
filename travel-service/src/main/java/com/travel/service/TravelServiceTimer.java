cf71f3de4912903267cd641e108acf9585b21384
chenzp <847979339@qq.com>
2018-05-21 15:22:01 +0800

    详情查询

--- travel-service/src/main/java/com/travel/service/TravelServiceTimer.java
+++ travel-service/src/main/java/com/travel/service/TravelServiceTimer.java
@@ -1,10 +1,15 @@
 package com.travel.service;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.travel.common.HttpTools;
+import com.travel.dao.TravelMapper;
+import com.travel.pojo.Travel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,9 +21,25 @@ import java.util.regex.Pattern;
 @Component
 public class TravelServiceTimer {
 
+    @Autowired
+    private TravelMapper travelMapper;
+
     @Scheduled(cron="0/5 * *  * * ? ")   //每5秒执行一次
-    public void getTravelInfo(){
-        System.out.println("进入测试");
+    public void getTravelInfo() throws Exception {
+        String b = new HttpTools().getString("https://lvyou.baidu.com/destination/ajax/jingdian?format=ajax&cid=0&playid=0&seasonid=5&surl=sichuan&pn=1&rn=18", "");
+        JSONObject jsonObject = JSONObject.parseObject(b);
+        JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("scene_list");
+        for(int i = 0; i < jsonArray.size();i++){
+            JSONObject record = jsonArray.getJSONObject(i);
+            String travelName = record.getString("ambiguity_sname");  //地名
+            String province = record.getString("province");  //地名
+            String desc = record.getJSONObject("ext").getString("more_desc");
+            Travel travel = new Travel();
+            travel.setContent(desc);
+            travel.setName(travelName);
+            travel.setCreateTime(new Date());
+            travelMapper.insertSelective(travel);
+        }
     }
 
     public static String unicodeToString(String str) {
@@ -32,11 +53,4 @@ public class TravelServiceTimer {
         return str;
     }
 
-
-    public static void main(String[] args) throws Exception {
-        String b = new HttpTools().getString("https://lvyou.baidu.com/destination/ajax/jingdian?format=ajax&cid=0&playid=0&seasonid=5&surl=sichuan&pn=100&rn=18", "");
-        JSONObject jsonObject = JSONObject.parseObject(b);
-        String desc = jsonObject.getJSONObject("data").getJSONArray("scene_list").getJSONObject(0).getJSONObject("ext").getString("more_desc");
-        System.out.println(desc);
-    }
 }
