011d926ba14b614147f3115c730a487805001f0a
chenzp <847979339@qq.com>
2018-08-13 19:47:31 +0800

    注册流程

--- travel-pc/src/main/java/com/travel/controller/UserController.java
+++ travel-pc/src/main/java/com/travel/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.travel.controller;
 
+import com.travel.pojo.User;
+import com.travel.service.EmailService;
+import com.travel.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +18,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${contextpath}")
+    private String contextpath;
+
     @RequestMapping("login")
     @ResponseBody
-    public String login(){
+    public Object login(User user){
+        user = userService.login(user);
+        Integer status = user.getStatus();
+        switch (status){
+            case 2:  //未激活 发送邮件
+                emailService.sendText(user.getEmail(),"52trip账号激活",contextpath+"user/active?uuid="+user.getId());
+                break;
+            case 1:
+        }
+
         return "user/login";
     }
 }
--- travel-service/src/main/java/com/travel/service/UserService.java
+++ travel-service/src/main/java/com/travel/service/UserService.java
@@ -1,11 +1,13 @@
 package com.travel.service;
 
+import com.travel.common.StringUtil;
 import com.travel.dao.UserMapper;
 import com.travel.pojo.User;
 import com.travel.pojo.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,9 +26,14 @@ public class UserService {
         userExample.createCriteria().andEmailEqualTo(user.getEmail());
         List<User> users = userMapper.selectByExample(userExample);
         if(users.isEmpty()){// 没有注册  插入数据库  发送发送邮件的消息
+            String uuid = StringUtil.getUuid();
+            user.setId(uuid);
+            user.setName(user.getEmail());
+            user.setStatus(2); //已注册  没激活
+            user.setCreateTime(new Date());
             userMapper.insertSelective(user);
             return user;
         }
-        return user;
+        return users.get(0);
     }
 }
